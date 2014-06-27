def3fcb7425c3366c5793629278be569e5dd34dd
Alexander Kubenskiy <akoubensky@gmail.com>
2014-06-27 20:54:46 +0400

    Refactoring

--- Lecture09/src/RedBlackTree.java
+++ Lecture09/src/RedBlackTree.java
@@ -5,9 +5,6 @@ import java.util.Arrays;
  * Реализация основной операции красно-черного дерева -
  * операции вставки новой ассоциативной пары <ключ, значение>.
  * 
- * Операция удаления по ключу не реализована, так как корректировка
- * дерева после удаления производится сложнее, чем при вставке.
- * 
  * (c) http://algs4.cs.princeton.edu/33balanced/RedBlackBST.java.html
  *
  * @param <K> тип ключа
@@ -19,17 +16,6 @@ public class RedBlackTree<K extends Comparable<K>, V> {
 	 */
 	private static enum Color {
 		RED, BLACK;
-		
-		Color flip() {
-			switch (this) {
-			case RED: return BLACK;
-			case BLACK: return RED;
-			}
-			return null;
-		}
-		
-		boolean isRed() { return this == RED; }
-		boolean isBlack() { return this == BLACK; }
 	}
 
 	/**
@@ -86,35 +72,59 @@ public class RedBlackTree<K extends Comparable<K>, V> {
 	
 	/**
 	 * Добавление в дерево новой ассоциативной пары.
-	 * @param key ключ.
-	 * @param value значение.
-	 * @return значение, которое было ассоциировано раньше с этим ключом
+	 * @param key	Ключ.
+	 * @param value Значение.
+	 * @return		Значение, которое было ассоциировано раньше с этим ключом
 	 *         (если такое значение было).
 	 */
 	public V put(K key, V value) {
 		// Проверка: ключ поиска не должен быть пустым.
 		if (key == null) throw new NullPointerException("null key");
 
+		// Сначала ищем старое значение
 		V oldValue = get(key);
 		root = put(key, value, root);
+		// После вставки корень дерева может оказаться красным - перекрасим его.
 		root.color = Color.BLACK;
 		return oldValue;
 	}
 	
+	/**
+	 * Удаление ассоциативной пары из дерева по заданному ключу.
+	 * @param key	Ключ поиска (не равен null)
+	 * @return		Удаленное значение (или null, если такого ключа не было в дереве)
+	 */
 	public V remove(K key) {
 		// Проверка: ключ поиска не должен быть пустым.
 		if (key == null) throw new NullPointerException("null key");
 
+		// Пустое дерево - специальный случай.
 		if (root == SENTINEL) return null;
+		
+		// Сначала ищем старое значение
 		V oldValue = get(key);
-		if (root.color.isBlack() && root.left.color.isBlack() && root.right.color.isBlack()) {
+		
+		// Если корень дерева и оба его непосредственных потомка - черные, то временно
+		// сделаем корень красным
+		if (isBlack(root) && isBlack(root.left) && isBlack(root.right)) {
 			root.color = Color.RED;
 		}
 		root = remove(key, root);
+		// После удаления корень дерева может оказаться красным - перекрасим его.
 		root.color = Color.BLACK;
 		return oldValue;
 	}
 	
+	//----------------------------------------------------------------------------------
+	// Вспомогательные рекурсивные функции
+	//----------------------------------------------------------------------------------
+	
+	/**
+	 * Стандартный двоичный поиск в дереве по ключу
+	 * @param key	Ключ поиска
+	 * @param node	Начальный корень
+	 * @return
+	 */
 	private V get(K key, Node node) {
 		while (node != SENTINEL) {
 			int cmp = key.compareTo(node.key);
@@ -126,32 +136,60 @@ public class RedBlackTree<K extends Comparable<K>, V> {
 		return null;
 	}
 	
+	/**
+	 * Вставка в дерево новой ассоциативной пары.
+	 * @param key
+	 * @param value
+	 * @param node
+	 * @return
+	 */
 	private Node put(K key, V value, Node node) {
 		if (node == SENTINEL) {
+			// Новый вставляемый узел всегда красный - это не нарушает баланс черных и красных узлов
 			return new Node(key, value);
 		}
 		int cmp = key.compareTo(node.key);
-		if (cmp < 0) node.left = put(key, value, node.left);
-		else if (cmp > 0) node.right = put(key, value, node.right);
-		else node.value = value;
+		if (cmp < 0) {
+			node.left = put(key, value, node.left);
+		} else if (cmp > 0) {
+			node.right = put(key, value, node.right);
+		} else {
+			node.value = value;
+		}
 		
-		return balance(node);
+		// После вставки может оказаться нарушенной балансировка дерева - 
+		// у красного узла может образоваться красный потомок.
+		
+		// 1. Если красный узел справа, то перевешиваем его так, стобы красный узел был слева;
+		//    корень становится черным.
+        if (isRed(node.right) && isBlack(node.left)) {
+        	node = pivotLeft(node);
+        }
+        
+        // 2. Если есть два рядом расположенных красных узла, то делаем обратный поворот,
+        //    чтобы оба красных узла были потомками узла node.
+        if (isRed(node.left)  && isRed(node.left.left)) node = pivotRight(node);
+        
+        // 3. Если теперь оба потомка красные, то продвигаем красноту наверх, перекрашивая узлы.
+        if (isRed(node.left)  && isRed(node.right))     flipColors(node);
+        
+        return node;
 	}
 	
 	private Node remove(K key, Node node) {
         if (key.compareTo(node.key) < 0)  {
-            if (node.left.color.isBlack() && node.left.left.color.isBlack()) {
+            if (isBlack(node.left) && isBlack(node.left.left)) {
             	node = moveRedLeft(node);
             }
             node.left = remove(key, node.left);
         } else {
-            if (node.left.color.isRed()) {
+            if (isRed(node.left)) {
             	node = pivotRight(node);
             }
             if (key.compareTo(node.key) == 0 && (node.right == SENTINEL)) {
                 return SENTINEL;
             }
-            if (node.right.color.isBlack() && node.right.left.color.isBlack()) {
+            if (isBlack(node.right) && isBlack(node.right.left)) {
                 node = moveRedRight(node);
             }
             if (key.compareTo(node.key) == 0) {
@@ -169,12 +207,27 @@ public class RedBlackTree<K extends Comparable<K>, V> {
         return balance(node);
 	}
 	
+	private boolean isRed(Node node) {
+		return node.color == Color.RED;
+	}
+	
+	private boolean isBlack(Node node) {
+		return node.color == Color.BLACK;
+	}
+	
+	private void flipColor(Node node) {
+		switch (node.color) {
+		case RED: node.color = Color.BLACK; break;
+		case BLACK: node.color = Color.RED; break;
+		}
+	}
+	
     // delete the key-value pair with the minimum key rooted at h
     private Node deleteMin(Node node) { 
         if (node.left == SENTINEL)
             return SENTINEL;
 
-        if (node.left.color.isBlack() && node.left.left.color.isBlack())
+        if (isBlack(node.left) && isBlack(node.left.left))
             node = moveRedLeft(node);
 
         node.left = deleteMin(node.left);
@@ -185,13 +238,13 @@ public class RedBlackTree<K extends Comparable<K>, V> {
     private Node balance(Node node) {
         // assert (h != null);
 
-        if (node.right.color.isRed()) {
+        if (isRed(node.right)) {
         	node = pivotLeft(node);
         }
-        if (node.left.color.isRed() && node.left.left.color.isRed()) {
+        if (isRed(node.left) && isRed(node.left.left)) {
         	node = pivotRight(node);
         }
-        if (node.left.color.isRed() && node.right.color.isRed()) {
+        if (isRed(node.left) && isRed(node.right)) {
         	flipColors(node);
         }
 
@@ -206,7 +259,7 @@ public class RedBlackTree<K extends Comparable<K>, V> {
 	 * @return		Корень поддерева после поворота
 	 */
 	private Node pivotLeft(Node node) {
-		assert node != SENTINEL && node.right.color == Color.RED;
+		assert node != SENTINEL && isRed(node.right);
 		
 		// Перевешиваем ссылки
 		Node child = node.right;
@@ -228,9 +281,9 @@ public class RedBlackTree<K extends Comparable<K>, V> {
 	 * @return		Корень поддерева после поворота
 	 */
 	private Node pivotRight(Node node) {
-		assert node != SENTINEL && node.left.color == Color.RED;
+		assert node != SENTINEL && isRed(node.left);
 		
-		// Перевешиваем сслки
+		// Перевешиваем ссылки
 		Node child = node.left;
 		node.left = child.right;
 		child.right = node;
@@ -242,15 +295,19 @@ public class RedBlackTree<K extends Comparable<K>, V> {
 		return child;
 	}
 	
-    // flip the colors of a node and its two children
+    /**
+     * Перекрашивает в противоположные цвета заданный узел и двоих его потомков.
+     * Предполагается, что эти потомки имели одинаковый цвет и этот цвет не тот,
+     * что у заданного узла. 
+     * @param node	Узел для перекрашивания
+     */
     private void flipColors(Node node) {
-        // h must have opposite color of its two children
-        // assert (h != null) && (h.left != null) && (h.right != null);
-        // assert (!isRed(h) &&  isRed(h.left) &&  isRed(h.right))
-        //     || (isRed(h)  && !isRed(h.left) && !isRed(h.right));
-        node.color = node.color.flip();
-        node.left.color = node.left.color.flip();
-        node.right.color = node.right.color.flip();
+        assert node != SENTINEL && node.left != SENTINEL && node.right != SENTINEL;
+        assert node.left.color == node.right.color && node.left.color != node.color;
+
+        flipColor(node);
+        flipColor(node.left);
+        flipColor(node.right);
     }
 
     // Assuming that h is red and both h.left and h.left.left
@@ -260,7 +317,7 @@ public class RedBlackTree<K extends Comparable<K>, V> {
         // assert isRed(h) && !isRed(h.left) && !isRed(h.left.left);
 
         flipColors(node);
-        if (node.right.left.color.isRed()) { 
+        if (isRed(node.right.left)) { 
             node.right = pivotRight(node.right);
             node = pivotLeft(node);
         }
@@ -273,7 +330,7 @@ public class RedBlackTree<K extends Comparable<K>, V> {
         // assert (h != null);
         // assert isRed(h) && !isRed(h.right) && !isRed(h.right.left);
         flipColors(node);
-        if (node.left.left.color.isRed()) { 
+        if (isRed(node.left.left)) { 
             node = pivotRight(node);
         }
         return node;
